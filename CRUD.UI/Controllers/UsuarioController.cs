4cff422df40cb4ed44e418ae13a45e3963c97871
René López <rene.lopezdev@gmail.com>
2026-07-02 19:10:06 -0600

    Clase #8

--- CRUD.UI/Controllers/UsuarioController.cs
+++ CRUD.UI/Controllers/UsuarioController.cs
@@ -98,6 +98,37 @@ namespace CRUD.UI.Controllers
 
         }
 
+        [HttpPost]
+        public Respuesta<UsuarioDTO> ActualizarUsuarioPorId([FromBody] UsuarioDTO ObjUsuario)
+        {
+
+            Respuesta<UsuarioDTO> respuesta = new Respuesta<UsuarioDTO>();
+
+            try
+            {
+
+                var resBLL = _MantUsuarioBLL.ActualizarUsuarioPorId(ObjUsuario);
+
+                if (resBLL != null)
+                {
+
+                    respuesta = resBLL;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                respuesta.Ok = false;
+                respuesta.Mensaje = $"Ha ocurrido un error en el método ActualizarUsuarioPorId en el controlador de Usuario {ex.Message}";
+                respuesta.ValorRetorno = null;
+            }
+
+            return respuesta;
+
+        }
+
         public Respuesta<List<UsuarioDTO>> ObtenerUsuarios()
         {
 
@@ -122,6 +153,37 @@ namespace CRUD.UI.Controllers
 
                 respuesta.Ok = false;
                 respuesta.Mensaje = $"Ha ocurrido un error en la capa UsuarioController en el método ObtenerUsuarios {ex.Message}";
+                respuesta.ValorRetorno = null;
+            }
+
+            return respuesta;
+
+
+        }
+
+        public Respuesta<UsuarioDTO> ObtenerUsuarioPorId([FromBody] UsuarioDTO ObjUsuario)
+        {
+
+            Respuesta<UsuarioDTO> respuesta = new Respuesta<UsuarioDTO>();
+
+            try
+            {
+
+                var ResBLL = _MantUsuarioBLL.ObtenerUsuarioPorId(ObjUsuario);
+
+                if (ResBLL != null)
+                {
+                    respuesta = ResBLL;
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                respuesta.Ok = false;
+                respuesta.Mensaje = $"Ha ocurrido un error en la capa UsuarioController en el método ObtenerUsuarioPorId {ex.Message}";
                 respuesta.ValorRetorno = null;
             }
 
--- CRUD.UI/wwwroot/js/MantUsuarioJS/MantUsuario.js
+++ CRUD.UI/wwwroot/js/MantUsuarioJS/MantUsuario.js
@@ -30,6 +30,21 @@ jsMantUsuario = {
         InputDireccion: '#InputDireccion',
 
 
+        //Modal Editar Usuarios
+
+        btnObtenerUsuarioPorId: '.btnObtenerUsuarioPorId',
+        ModalInputIdUsuario: '#ModalInputIdUsuario',
+        ModalInputCorreo: '#ModalInputCorreo',
+        ModalInputContrasena: '#ModalInputContrasena',
+        ModalInputRol: '#ModalInputRol',
+        ModalInputNombre: '#ModalInputNombre',
+        ModalInputApellido1: '#ModalInputApellido1',
+        ModalInputApellido2: '#ModalInputApellido2',
+        ModalInputFechaNac: '#ModalInputFechaNac',
+        ModalInputGenero: '#ModalInputGenero',
+        ModalInputTelefono: '#ModalInputTelefono',
+        ModalInputDireccion: '#ModalInputDireccion',
+
 
 
 
@@ -54,7 +69,7 @@ jsMantUsuario = {
 
         btnCrearUsuario: '#btnCrearUsuario',
         btnCancelar: '#btnCancelar',
-
+        ModalbtnGuardarUsuario: '#ModalbtnGuardarUsuario',
 
     },
 
@@ -170,7 +185,196 @@ jsMantUsuario = {
 
         },
 
+        ObtenerUsuarioPorId: function (event) {
 
+            try {
+
+                let button = $(event.currentTarget);
+
+                let IdUsuarioBoton = button.data("idusuario")
+
+                let ObjUsuario = {
+
+                    IdUsuario: IdUsuarioBoton
+
+                };
+
+                fetch('../Usuario/ObtenerUsuarioPorId', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(ObjUsuario)
+                })
+                    .then(respuesta => respuesta.json())
+                    .then(resultado => {
+
+                        console.log(resultado)
+
+
+                        if (resultado.ok) {
+
+
+                            $(jsMantUsuario.controles.ModalInputIdUsuario).val(resultado.valorRetorno.idUsuario)
+                            $(jsMantUsuario.controles.ModalInputCorreo).val(resultado.valorRetorno.correo)
+                            $(jsMantUsuario.controles.ModalInputContrasena).val(resultado.valorRetorno.contrasena)
+                            $(jsMantUsuario.controles.ModalInputRol).val(resultado.valorRetorno.idRol)
+                            $(jsMantUsuario.controles.ModalInputNombre).val(resultado.valorRetorno.nombre)
+                            $(jsMantUsuario.controles.ModalInputApellido1).val(resultado.valorRetorno.apellido1)
+                            $(jsMantUsuario.controles.ModalInputApellido2).val(resultado.valorRetorno.apellido2)
+                            $(jsMantUsuario.controles.ModalInputFechaNac).val(resultado.valorRetorno.fechaNacimiento)
+                            $(jsMantUsuario.controles.ModalInputGenero).val(resultado.valorRetorno.genero)
+                            $(jsMantUsuario.controles.ModalInputTelefono).val(resultado.valorRetorno.telefono)
+                            $(jsMantUsuario.controles.ModalInputDireccion).val(resultado.valorRetorno.direccion)
+
+                        } else {
+
+                            Swal.fire({
+                                title: "Advertencia",
+                                text: `${resultado.mensaje}`,
+                                icon: "warning",
+                                confirmButtonText: "Entendido",
+                                confirmButtonColor: "#297ea6"
+                            });
+
+                        }
+
+
+
+
+                    })
+
+
+            } catch (e) {
+                console.log("Ha ocurrido un error al Obtener el usuario, en el método ObtenerUsuarioPorId en el JS")
+            }
+
+
+        },
+
+        ActualizarUsuarioPorId: function () {
+
+            event.preventDefault();
+
+
+            try {
+
+
+                let ObjUsuario = {
+
+                    IdUsuario: $(jsMantUsuario.controles.ModalInputIdUsuario).val(),
+                    Correo: $(jsMantUsuario.controles.ModalInputCorreo).val(),
+                    Contrasena: $(jsMantUsuario.controles.ModalInputContrasena).val(),
+                    IdRol: $(jsMantUsuario.controles.ModalInputRol).val(),
+                    Nombre: $(jsMantUsuario.controles.ModalInputNombre).val(),
+                    Apellido1: $(jsMantUsuario.controles.ModalInputApellido1).val(),
+                    Apellido2: $(jsMantUsuario.controles.ModalInputApellido2).val(),
+                    FechaNacimiento: $(jsMantUsuario.controles.ModalInputFechaNac).val(),
+                    Genero: $(jsMantUsuario.controles.ModalInputGenero).val(),
+                    Telefono: $(jsMantUsuario.controles.ModalInputTelefono).val(),
+                    Direccion: $(jsMantUsuario.controles.ModalInputDireccion).val(),
+
+
+                };
+
+
+                if (ObjUsuario.Correo !== "" && ObjUsuario.Contrasena !== "" && ObjUsuario.IdRol !== "0" && ObjUsuario.Nombre !== "" && ObjUsuario.Apellido1 !== "" &&
+                    ObjUsuario.Apellido2 !== "" && ObjUsuario.FechaNacimiento !== "" && ObjUsuario.Genero !== "0" && ObjUsuario.Telefono !== "" && ObjUsuario.Direccion !== "") {
+
+
+                    Swal.fire({
+                        title: "¿Estás seguro que quieres editar este usuario?",
+                        text: "Esta acción es irreversible",
+                        icon: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#297ea6",
+                        cancelButtonColor: "#d33",
+                        confirmButtonText: "Sí, Editar",
+                        cancelButtonText: 'No'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+
+
+                            fetch('../Usuario/ActualizarUsuarioPorId', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify(ObjUsuario)
+                            })
+                                .then(respuesta => respuesta.json())
+                                .then(resultado => {
+
+
+
+                                    Swal.fire({
+                                        title: "Éxito",
+                                        text: `${resultado.mensaje}`,
+                                        icon: "success",
+                                        confirmButtonText: "Entendido",
+                                        confirmButtonColor: "#297ea6"
+                                    });
+
+                                    setTimeout(() => {
+
+                                        window.location.reload();
+
+                                    }, 2100)
+
+
+
+
+                                })
+
+
+
+                        } else {
+
+                            Swal.fire({
+                                title: "Advertencia",
+                                text: `${resultado.mensaje}`,
+                                icon: "warning",
+                                confirmButtonText: "Entendido",
+                                confirmButtonColor: "#297ea6"
+                            });
+
+                        }
+
+
+
+
+
+                    });
+
+
+
+
+
+                } else {
+
+                    Swal.fire({
+                        title: "Advertencia",
+                        text: `Debe rellenar todos los campos`,
+                        icon: "warning",
+                        confirmButtonText: "Entendido",
+                        confirmButtonColor: "#297ea6"
+                    });
+
+                }
+
+
+
+
+            } catch (e) {
+
+                console.warn("Ha ocurrido un error en la función ActualizarUsuarioPorId en el JS: ", e)
+
+
+            }
+
+
+
+        },
 
 
 
@@ -184,9 +388,18 @@ jsMantUsuario = {
 
             });
 
+            $(jsMantUsuario.controles.btnObtenerUsuarioPorId).on('click', function (event) {
+
+                jsMantUsuario.metodos.ObtenerUsuarioPorId(event);
+
+            });
 
 
+            $(jsMantUsuario.botones.ModalbtnGuardarUsuario).on('click', function () {
 
+                jsMantUsuario.metodos.ActualizarUsuarioPorId();
+
+            });
 
         }
 
